38ccf37f653e1d0ac874d00b1abcb5818f6482fd
Andrej Kabachnik <andrej_ka@gmx.de>
2016-09-22 11:53:20 +0200

    NEW implemented the uninstaller

--- ModxCmsConnectorApp.php
+++ ModxCmsConnectorApp.php
@@ -12,7 +12,7 @@ class ModxCmsConnectorApp extends \exface\Core\CommonLogic\AbstractApp {
 		try {
 			$this->get_workbench()->filemanager()->copy(
 				$this->get_directory_absolute_path() . DIRECTORY_SEPARATOR . 'modx' . DIRECTORY_SEPARATOR . 'index-exface.php', 
-				$this->get_workbench()->get_installation_path() . DIRECTORY_SEPARATOR . $this->get_config()->get_option('PATH_TO_MODX'), 
+				$this->get_modx_ajax_index_path(), 
 				true
 			);
 			$result .= 'Updated index-exface.php in MODx';
@@ -20,6 +20,20 @@ class ModxCmsConnectorApp extends \exface\Core\CommonLogic\AbstractApp {
 			throw $e;
 		}
 		return $result;
+	}
+	
+	/**
+	 * 
+	 * {@inheritDoc}
+	 * @see \exface\Core\CommonLogic\AbstractApp::uninstall()
+	 */
+	public function uninstall(){
+		// Remove index-exface.php to the root of the MODx installation
+		$this->get_workbench()->filemanager()->remove($this->get_modx_ajax_index_path());
+	}
+	
+	public function get_modx_ajax_index_path(){
+		return $this->get_workbench()->get_installation_path() . DIRECTORY_SEPARATOR . $this->get_config()->get_option('PATH_TO_MODX');
 	}
 }
 ?>
