d63906e93cb10692b155a7b83f7f0e69d25767be
Andrej Kabachnik <andrej_ka@gmx.de>
2017-01-19 17:03:14 +0100

    NEW AppInstaller framework

--- ModxCmsConnectorApp.php
+++ ModxCmsConnectorApp.php
@@ -2,36 +2,11 @@
 class ModxCmsConnectorApp extends \exface\Core\CommonLogic\AbstractApp {
 	
 	/**
+	 * Returns the absolute path to the index-exface.php file in the root folder of MODx. This file is used to initialize
+	 * MODx for API requests, that are not handled by MODx by default.
 	 * 
-	 * {@inheritDoc}
-	 * @see \exface\Core\CommonLogic\AbstractApp::install()
+	 * @return string
 	 */
-	public function install(){
-		$result = '';
-		// Copy index-exface.php to the root of the MODx installation
-		try {
-			$this->get_workbench()->filemanager()->copy(
-				$this->get_directory_absolute_path() . DIRECTORY_SEPARATOR . 'modx' . DIRECTORY_SEPARATOR . 'index-exface.php', 
-				$this->get_modx_ajax_index_path(), 
-				true
-			);
-			$result .= 'Updated index-exface.php in MODx';
-		} catch (\Exception $e){
-			throw $e;
-		}
-		return $result;
-	}
-	
-	/**
-	 * 
-	 * {@inheritDoc}
-	 * @see \exface\Core\CommonLogic\AbstractApp::uninstall()
-	 */
-	public function uninstall(){
-		// Remove index-exface.php to the root of the MODx installation
-		$this->get_workbench()->filemanager()->remove($this->get_modx_ajax_index_path());
-	}
-	
 	public function get_modx_ajax_index_path(){
 		return $this->get_workbench()->get_installation_path() . DIRECTORY_SEPARATOR . $this->get_config()->get_option('PATH_TO_MODX');
 	}
