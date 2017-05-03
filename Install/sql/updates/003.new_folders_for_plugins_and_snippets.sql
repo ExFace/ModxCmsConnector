6753242169b7328e80c2391c002a6c39964799bc
Andrej Kabachnik <andrej_ka@gmx.de>
2017-05-03 15:47:08 +0200

    FIX last sql update

--- Install/sql/updates/003.new_folders_for_plugins_and_snippets.sql
+++ Install/sql/updates/003.new_folders_for_plugins_and_snippets.sql
@@ -1,2 +1,2 @@
-UPDATE modx_site_plugins SET plugincode = 'require MODX_BASE_PATH.\'assets/plugins/../../exface/vendor/exface/ModxCmsConnector/modx/plugins/exface/plugin.exface.php\';' WHERE name = 'ExFace';
-UPDATE modx_site_snippets SET snippet = 'return require MODX_BASE_PATH.\'assets/snippets/../../exface/vendor/exface/ModxCmsConnector/modx/snippets/exface/snippet.exface.php\';' WHERE name = 'ExFace';
+UPDATE modx_site_plugins SET plugincode = 'require MODX_BASE_PATH.''assets/plugins/../../exface/vendor/exface/ModxCmsConnector/modx/plugins/exface/plugin.exface.php\''';' WHERE name = 'ExFace';
+UPDATE modx_site_snippets SET snippet = 'return require MODX_BASE_PATH.'''assets/snippets/../../exface/vendor/exface/ModxCmsConnector/modx/snippets/exface/snippet.exface.php\''';' WHERE name = 'ExFace';
