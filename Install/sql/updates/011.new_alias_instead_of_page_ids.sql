4c7b27c48cad4f8a515613289d9f2a2252a818a6
aka <andrej.kabachnik@salt-solutions.de>
2017-11-01 17:40:03 +0100

    FIX SQL update 11

--- Install/sql/updates/011.new_alias_instead_of_page_ids.sql
+++ Install/sql/updates/011.new_alias_instead_of_page_ids.sql
@@ -8,15 +8,6 @@ UPDATE modx_site_plugins SET disabled = 1 WHERE name = 'TransAlias';
 
 #Exface Plugin aktivieren
 UPDATE modx_site_plugins SET disabled = 0, name = 'ExFace' WHERE name = 'ExFace User Connector' OR name = 'ExFace';
-#Exface Plugin mit OnDocFormSave Event verbinden
-INSERT INTO modx_site_plugin_events (pluginid, evtid, priority) VALUES
-((SELECT msp.id FROM modx_site_plugins msp WHERE msp.name = 'ExFace User Connector' OR msp.name = 'ExFace'), (SELECT msen.id FROM modx_system_eventnames msen WHERE msen.name = 'OnDocFormSave'), 1) ON DUPLICATE KEY UPDATE;
-
-#mm_rules setzen um ExfacePageUID readonly, ExfacePageDefaultParentAlias nicht sichtbar zu machen
-UPDATE modx_site_htmlsnippets SET snippet = CONCAT(snippet, '
-
-mm_ddReadonly(\'ExfacePageUID\');
-mm_ddReadonly(\'ExfacePageDefaultParentAlias\');') WHERE name = 'mm_rules';
 
 #Im Seiten-Content page_id durch page_alias ersetzen
 UPDATE modx_site_content SET content = REPLACE(content, 'page_id', 'page_alias');
