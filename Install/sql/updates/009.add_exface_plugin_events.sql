1d40919bf670939d3598a49a33d2f1c46deac2ce
Stefan Leupold <stefan.leupold@salt-solutions.de>
2017-11-28 09:54:27 +0100

    FIX Probleme beim Deployment

--- Install/sql/updates/009.add_exface_plugin_events.sql
+++
@@ -1,8 +0,0 @@
-#Exface Plugin aktivieren
-UPDATE modx_site_plugins SET disabled = 0, name = 'ExFace' WHERE name = 'ExFace User Connector' OR name = 'ExFace';
-#Exface Plugin mit OnDocFormSave Event verbinden
-INSERT INTO modx_site_plugin_events (pluginid, evtid, priority) VALUES
-((SELECT msp.id FROM modx_site_plugins msp WHERE msp.name = 'ExFace User Connector' OR msp.name = 'ExFace'), (SELECT msen.id FROM modx_system_eventnames msen WHERE msen.name = 'OnWebSaveUser'), 1),
-((SELECT msp.id FROM modx_site_plugins msp WHERE msp.name = 'ExFace User Connector' OR msp.name = 'ExFace'), (SELECT msen.id FROM modx_system_eventnames msen WHERE msen.name = 'OnWebDeleteUser'), 1),
-((SELECT msp.id FROM modx_site_plugins msp WHERE msp.name = 'ExFace User Connector' OR msp.name = 'ExFace'), (SELECT msen.id FROM modx_system_eventnames msen WHERE msen.name = 'OnBeforeUserFormSave'), 1),
-((SELECT msp.id FROM modx_site_plugins msp WHERE msp.name = 'ExFace User Connector' OR msp.name = 'ExFace'), (SELECT msen.id FROM modx_system_eventnames msen WHERE msen.name = 'OnBeforeWUsrFormSave'), 1);
