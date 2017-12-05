779fff3277ac6a21cd413d462a75ad9ebf91a619
Stefan Leupold <stefan.leupold@salt-solutions.de>
2017-12-05 10:43:59 +0100

    NEW Set important Modx settings in Installer

--- ModxCmsConnectorInstaller.php
+++ ModxCmsConnectorInstaller.php
@@ -20,14 +20,48 @@ class ModxCmsConnectorInstaller extends AbstractAppInstaller
      */
     public function install($source_absolute_path)
     {
-        $result = "\n";
         // Copy index-exface.php to the root of the MODx installation
         try {
             $this->getWorkbench()->filemanager()->copy($this->getApp()->getDirectoryAbsolutePath() . DIRECTORY_SEPARATOR . 'Install' . DIRECTORY_SEPARATOR . 'index-exface.php', $this->getApp()->getModxAjaxIndexPath(), true);
-            $result .= 'Updated index-exface.php in MODx';
+            $result .= "\nUpdated index-exface.php in MODx";
         } catch (\Exception $e) {
-            $result .= 'Failed to update index-exface.php in MODx' . $e->getMessage();
+            $result .= "\nFailed to update index-exface.php in MODx" . $e->getMessage();
         }
+        
+        try {
+            $modx = $this->getWorkbench()->getApp('exface.ModxCmsConnector')->getModx();
+        } catch (\Throwable $e) {
+            $result .= "\nError getting MODx";
+            return $result;
+        }
+        
+        // Update important Modx settings.
+        try {
+            // System settings
+            $systemSettings = $modx->getFullTableName('system_settings');
+            $modx->db->update(['setting_value' => '1'], $systemSettings, 'setting_name = "friendly_urls"');
+            $modx->db->update(['setting_value' => '0'], $systemSettings, 'setting_name = "allow_duplicate_alias"');
+            $modx->db->update(['setting_value' => '1'], $systemSettings, 'setting_name = "automatic_alias"');
+            $modx->db->update(['setting_value' => '1'], $systemSettings, 'setting_name = "udperms_allowroot"');
+            
+            // Plugins
+            $sitePlugins = $modx->getFullTableName('site_plugins');
+            $modx->db->update(['disabled' => 1], $sitePlugins, 'name = "TransAlias"');
+            $modx->db->update(['disabled' => 0], $sitePlugins, 'name = "ExFace"');
+            
+            // Default template
+            $siteTemplates = $modx->getFullTableName('site_templates');
+            $templateResult = $modx->db->select('id', $siteTemplates, 'templatename = "' . $this->getApp()->getConfig()->getOption('MODX.TEMPLATE_NAME_DEFAULT') . '"');
+            if ($modx->db->getRecordCount($templateResult) === 1) {
+                $templateRow = $modx->db->getRow($templateResult);
+                $modx->db->update(['setting_value' => $templateRow['id']], $systemSettings, 'setting_name = "default_template"');
+            }
+            
+            $result .= "\nUpdated MODx settings";
+        } catch (\Throwable $e) {
+            $result .= "\nError updating MODx settings;" . $e->getMessage();
+        }
+        
         return $result;
     }
 
