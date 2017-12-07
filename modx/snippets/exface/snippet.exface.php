ce1a800db4b23674ec9602bdeef59527736bc82e
Andrej Kabachnik <andrej_ka@gmx.de>
2017-12-07 17:51:35 +0100

    FIX error handling in exface snippet

--- modx/snippets/exface/snippet.exface.php
+++ modx/snippets/exface/snippet.exface.php
@@ -73,14 +73,14 @@ switch ($action) {
         try {
             $result = $template_instance->processRequest($docAlias, null, 'exface.Core.ShowHeaders', true);
         } catch (\exface\Core\Interfaces\Exceptions\ErrorExceptionInterface $e) {
-            $exface->getLogger()->log($e->getLogLevel(), $e->getMessage(), array(), $e);
+            $exface->getLogger()->logException($e);
             $ui = $exface->ui();
             $page = \exface\Core\Factories\UiPageFactory::create($ui, '');
             try {
                 $result = $template_instance->drawHeaders($e->createWidget($page));
             } catch (\Exception $ee) {
                 // If the exception widget cannot be rendered either, output no headers in order not to break them.
-                $exface->getLogger()->log($ee->getLogLevel(), $ee->getMessage(), array(), $ee);
+                $exface->getLogger()->logException($ee);
             }
         }
         break;
