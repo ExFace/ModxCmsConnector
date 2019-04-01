24cf0ee7d7632b21a43736f8f2002a9d4f5a685c
Andrej Kabachnik <andrej_ka@gmx.de>
2019-04-01 16:10:08 +0200

    FIX SQL update #16

--- Install/sql/updates/016.refactor_template_to_facade.sql
+++ Install/sql/updates/016.refactor_template_to_facade.sql
@@ -1,12 +1,11 @@
 /* Add the template parameter to all current templates */
-UPDATE modx_site_templates SET content = REPLACE(content, '[[ExFace? &template=', '[[ExFace? &facade=');
-UPDATE modx_site_templates SET content = REPLACE(content, '[!ExFace? &template=', '[!ExFace? &facade=');
+UPDATE modx_site_templates SET content = REPLACE(content, 'ExFace?  &facade=', 'ExFace? &facade=');
 
-UPDATE modx_site_templates SET content = REPLACE(content, 'JEasyUiTemplate', 'JEasyUIFacade');
-UPDATE modx_site_templates SET content = REPLACE(content, 'AdminLteTemplate', 'AdminLTEFacade');
-UPDATE modx_site_templates SET content = REPLACE(content, 'JQueryMobileTemplate', 'JQueryMobileFacade');
-UPDATE modx_site_templates SET content = REPLACE(content, 'NativeDroid2Template', 'NativeDroid2Facade');
-UPDATE modx_site_templates SET content = REPLACE(content, 'OpenUI5Template', 'UI5Facade');
+UPDATE modx_site_templates SET content = REPLACE(content, 'JEasyUiTemplate.JEasyUiTemplate', 'JEasyUIFacade.JEasyUIFacade');
+UPDATE modx_site_templates SET content = REPLACE(content, 'AdminLteTemplate.AdminLteTemplate', 'AdminLTEFacade.AdminLTEFacade');
+UPDATE modx_site_templates SET content = REPLACE(content, 'JQueryMobileTemplate.JQueryMobileTemplate', 'JQueryMobileFacade.JQueryMobileFacade');
+UPDATE modx_site_templates SET content = REPLACE(content, 'NativeDroid2Template.NativeDroid2Template', 'NativeDroid2Facade.NativeDroid2Facade');
+UPDATE modx_site_templates SET content = REPLACE(content, 'OpenUI5Template.OpenUI5Template', 'UI5Facade.UI5Facade');
 
 UPDATE modx_site_templates SET content = REPLACE(content, 'exface.AdminLTEFacade.html', 'exface.AdminLTETemplate.html');
 UPDATE modx_site_templates SET content = REPLACE(content, 'exface.JEasyUIFacade.html', 'exface.JEasyUITemplate.html');
