1c409e118d21ac81911257f8e854c9ddc903731f
aka <andrej.kabachnik@salt-solutions.de>
2019-09-27 11:18:22 +0200

    FIX importing entire tables via ModxDb connector

--- DataConnectors/ModxDb.php
+++ DataConnectors/ModxDb.php
@@ -3,6 +3,7 @@ namespace exface\ModxCmsConnector\DataConnectors;
 
 use exface\Core\CommonLogic\AbstractDataConnector;
 use exface\Core\DataConnectors\MySqlConnector;
+use exface\Core\Exceptions\DataSources\DataConnectionConfigurationError;
 
 /**
  * The MODx DB connector uses the same MySQL connection as MODx.
@@ -56,5 +57,153 @@ class ModxDb extends MySqlConnector
     {
         // Disconnect is handled by modx, not by ExFace, so no need to do anything here
     }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\Core\DataConnectors\MySqlConnector::getDbase()
+     */
+    public function getDbase()
+    {
+        return $this->getModx()->db->config['dbase'];
+    }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\Core\DataConnectors\AbstractSqlConnector::getHost()
+     */
+    public function getHost()
+    {
+        return $this->getModx()->db->config['host'];
+    }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\Core\DataConnectors\AbstractSqlConnector::getUser()
+     */
+    public function getUser()
+    {
+        return $this->getModx()->db->config['user'];
+    }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\Core\DataConnectors\AbstractSqlConnector::getPassword()
+     */
+    public function getPassword()
+    {
+        return $this->getModx()->db->config['pass'];
+    }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\Core\DataConnectors\AbstractSqlConnector::getCharacterSet()
+     */
+    public function getCharacterSet()
+    {
+        return $this->getModx()->db->config['charset'];
+    }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\Core\DataConnectors\MySqlConnector::getConnectionMethod()
+     */
+    public function getConnectionMethod()
+    {
+        return $this->getModx()->db->config['connection_method'];
+    }
+    
+    /**
+     * 
+     * @return string
+     */
+    public function getModxTablePrefix() : string
+    {
+        return $this->getModx()->db->config['table_prefix'];
+    }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\Core\DataConnectors\AbstractSqlConnector::setHost()
+     */
+    public function setHost($value)
+    {
+        throw new DataConnectionConfigurationError($this, 'Cannot set connection options for CMS DB connector: use the database configuration of the CMS!');
+    }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\Core\DataConnectors\MySqlConnector::setDbase()
+     */
+    public function setDbase($value)
+    {
+        throw new DataConnectionConfigurationError($this, 'Cannot set connection options for CMS DB connector: use the database configuration of the CMS!');
+    }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\Core\DataConnectors\AbstractSqlConnector::setUser()
+     */
+    public function setUser($value)
+    {
+        throw new DataConnectionConfigurationError($this, 'Cannot set connection options for CMS DB connector: use the database configuration of the CMS!');
+    }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\Core\DataConnectors\MySqlConnector::setUsePersistantConnection()
+     */
+    public function setUsePersistantConnection($value)
+    {
+        throw new DataConnectionConfigurationError($this, 'Cannot set connection options for CMS DB connector: use the database configuration of the CMS!');
+    }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\Core\DataConnectors\AbstractSqlConnector::setPassword()
+     */
+    public function setPassword($value)
+    {
+        throw new DataConnectionConfigurationError($this, 'Cannot set connection options for CMS DB connector: use the database configuration of the CMS!');
+    }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\Core\DataConnectors\MySqlConnector::setCharset()
+     */
+    public function setCharset($value)
+    {
+        throw new DataConnectionConfigurationError($this, 'Cannot set connection options for CMS DB connector: use the database configuration of the CMS!');
+    }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\Core\DataConnectors\AbstractSqlConnector::setCharacterSet()
+     */
+    public function setCharacterSet($value)
+    {
+        throw new DataConnectionConfigurationError($this, 'Cannot set connection options for CMS DB connector: use the database configuration of the CMS!');
+    }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\Core\DataConnectors\MySqlConnector::setConnectionMethod()
+     */
+    public function setConnectionMethod($value)
+    {
+        throw new DataConnectionConfigurationError($this, 'Cannot set connection options for CMS DB connector: use the database configuration of the CMS!');
+    }
 }
-?>
